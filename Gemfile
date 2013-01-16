f7c2a610531644ff0814c895dc87f38ce20e2fd2
Kurt Nelson <kurtisnelson@gmail.com>
2013-01-16 16:18:59 -0500

    move to S3

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem "therubyracer"
 gem "rack-rewrite"
 gem "redcarpet"
 gem "middleman", "~>3.0.6"
+gem "middleman-sync", "~> 3.0.8"
 gem "thin"
 
 group :development do
--- config.rb
+++ config.rb
@@ -71,3 +71,15 @@ configure :build do
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
+
+# Activate sync extension
+activate :sync do |sync|
+  sync.fog_provider = 'AWS' # Your storage provider
+  sync.fog_directory = 'gtccf.org' # Your bucket name
+  sync.fog_region = 'us-east-1' # The region your storage bucket is in (eg us-east-1, us-west-1, eu-west-1, ap-southeast-1 )
+  sync.aws_secret_access_key = ENV['AWS_SECRET']
+  sync.aws_access_key_id = ENV['AWS_ID']
+  sync.existing_remote_files = 'delete' # What to do with your existing remote files? ( keep or delete )
+  # sync.gzip_compression = false # Automatically replace files with their equivalent gzip compressed version
+  sync.after_build = false # Disable sync to run after Middleman build ( defaults to true )
+end
