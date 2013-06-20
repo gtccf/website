4fe2b8fdf80e43da94c92e2b2972f2909f5002d6
Kurt Nelson <kurtisnelson@gmail.com>
2013-06-20 00:35:14 -0400

    new devise DB formate

--- db/schema.rb
+++ db/schema.rb
@@ -25,16 +25,26 @@ ActiveRecord::Schema.define(version: 20130620031421) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                              default: "", null: false
-    t.string   "encrypted_password",     limit: 128, default: "", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "password_salt"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        default: 0
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.string   "authentication_token"
+    t.string   "invitation_token"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin"
