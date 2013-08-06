8b60f18ea8f184d3ee3bca8151603b82762e55d3
Kurt Nelson <kurtisnelson@gmail.com>
2013-08-06 10:22:07 -0400

    Fall update

--- app/controllers/devotions_controller.rb
+++ app/controllers/devotions_controller.rb
@@ -16,14 +16,8 @@ class DevotionsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_devotion
-      @devotion = Devotion.find(params[:id])
-      @devotion = DevotionDecorator.decorate(@devotion)
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def devotion_params
-      params.require(:devotion).permit(:title, :release_date, :body)
-    end
+  def set_devotion
+    @devotion = Devotion.find(params[:id])
+    @devotion = DevotionDecorator.decorate(@devotion)
+  end
 end
