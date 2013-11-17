dc3fdad193f48fdd5e3d2d4d68aba466b5fca0f9
Kurt Nelson <kurtisnelson@gmail.com>
2013-11-16 22:27:50 -0500

    Better event scoping

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,11 +2,7 @@ class Event < ActiveRecord::Base
   has_and_belongs_to_many :tags
   accepts_nested_attributes_for :tags
 
-  def self.tagged_with(name)
-    EventDecorator.decorate_collection(Tag.find_by_name!(name).events)
-  end
-
-  def self.next(i)
-    EventDecorator.decorate_collection(Event.order(:time).limit(i))
-  end
+  scope :future, -> { where('time >= ?', Time.now) }
+  scope :next, ->(i) { future.order(:time).limit(i) }
+  scope :tagged, ->(name) { joins(:tags).where('tags.name = ?', name) }
 end
