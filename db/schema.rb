f9e91e4d1ea298e154b3e2cae0d8c6cf7a5b97b5
Kurt Nelson <kurtisnelson@gmail.com>
2014-01-21 14:19:21 -0500

    Update ruby version

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130922162720) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "devotions", force: true do |t|
     t.string   "title"
     t.date     "release_date"
