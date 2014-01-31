15b0ab7e785343e7728dc4842e1015992f406e25
Kurt Nelson <kurtisnelson@gmail.com>
2014-01-31 15:45:37 -0500

    Event enabled flag

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140131193546) do
+ActiveRecord::Schema.define(version: 20140131202104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20140131193546) do
     t.datetime "updated_at"
     t.string   "short_description"
     t.text     "recurring"
+    t.boolean  "enabled",           default: true
   end
 
   create_table "events_tags", id: false, force: true do |t|
