16ed780444fac9f5cea2d243960dce4595b461c5
Kurt Nelson <kurtisnelson@gmail.com>
2014-02-01 17:08:46 -0500

    Add end time to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140131202104) do
+ActiveRecord::Schema.define(version: 20140201220748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20140131202104) do
     t.string   "short_description"
     t.text     "recurring"
     t.boolean  "enabled",           default: true
+    t.time     "end_time"
   end
 
   create_table "events_tags", id: false, force: true do |t|
